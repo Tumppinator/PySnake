398eb4ea1b932af851b8c6036d1e30c0c7b641fc
Tumppinator <juutumppi123@gmail.com>
2025-11-28 19:17:15 +0200

    Added functionality to save and load high score from a file

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,8 @@ class ScoreBoard(Turtle):
 
     def __init__(self):
         super().__init__()
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.score = 0
         self.color("white")
         self.hideturtle()
@@ -15,16 +17,22 @@ class ScoreBoard(Turtle):
         self.goto(SCORE_POSITION[0], SCORE_POSITION[1])
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"GAME OVER ", align=ALIGNMENT, font=FONT)
+
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score is: {self.score} ", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score is: {self.score} High Score: {self.high_score} ", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
 
